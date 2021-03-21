897d11c12f690c84d8ad0906fce8dd7b1c521ea1
Keith Levi <guilddrive@gmail.com>
2021-03-21 09:54:31 -0500

    Update functionAndArrayTests.js

--- mochaTests/labW1D1/functionAndArrayTests.js
+++ mochaTests/labW1D1/functionAndArrayTests.js
@@ -6,11 +6,23 @@ describe("maxOfThree", function () {
     it("tests 1 2 3", function () {
         assert.strictEqual(maxOfThree(1, 2, 3), 3);
     });
+    it("tests 1 3 2", function () {
+        assert.strictEqual(maxOfThree(10, 30, 20), 30);
+    });
+    it("tests 2 1 3", function () {
+        assert.strictEqual(maxOfThree(2, 1, 3), 3);
+    });
+    it("tests 2 3 1", function () {
+        assert.strictEqual(maxOfThree(22, 33, 11), 33);
+    });
     it("tests 3 2 1", function () {
         assert.strictEqual(maxOfThree(3, 2, 1), 3);
     });
+    it("tests 3 1 2", function () {
+        assert.strictEqual(maxOfThree(333, 111, 222), 333);
+    });
     it("tests -1 -2 -3", function () {
-        assert.strictEqual(maxOfThree(-1, -2, -3), 3);
+        assert.strictEqual(maxOfThree(-1, -2, -3), -1);
     });
 });
 
