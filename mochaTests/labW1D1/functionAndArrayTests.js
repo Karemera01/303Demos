d8636ba4601eb288d692d3f7611953cdf092d7a7
Keith Levi <guilddrive@gmail.com>
2021-03-22 13:44:16 -0500

    tests that work in Node and browser

--- mochaTests/labW1D1/functionAndArrayTests.js
+++ mochaTests/labW1D1/functionAndArrayTests.js
@@ -1,4 +1,15 @@
 "use strict";
+
+/* You need the assert and function name declarations to test in node.  
+Comment these out when you send it to the browser with the index.html mocha setup page.
+*/
+const assert = require("assert");  //always need this with node
+const myExports = require("./functions.js");  //with node need the name of your file with your functions here
+const maxOfThree = myExports.maxOfThree;  //do this for all of the functions used in the Mocha tests
+const multiply = myExports.multiply;
+const sum = myExports.sum;
+
+
 /* global assert maxOfThree sum multiply findLongestWord reverseArray reverseArrayInPlace scoreExams */
 
 /* 1.	1.	Define a function maxOfThree() that takes three numbers as arguments and returns the largest of them.  */
--- mochaTests/labW1D1/functions.js
+++ mochaTests/labW1D1/functions.js
@@ -1,6 +1,7 @@
 "use strict";
 
-
+/* You need the module.exports when testing in node.  Comment it out when you send your file to the browser */
+module.exports = {maxOfThree, sum, multiply }; 
 
 /**
  * 
