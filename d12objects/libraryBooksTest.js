d8a8f37ef84f1727c2cc71494fc40f9dec2895b2
KeithLevi <guilddrive@gmail.com>
2021-03-24 14:14:55 -0500

    afternoon updates 214pm

--- d12objects/libraryBooksTest.js
+++ d12objects/libraryBooksTest.js
@@ -37,18 +37,18 @@ describe("library", function () {
     });
 
     it("find authors", function () {
-        assert.deepStrictEqual(findAuthors(), authors);
+        assert.deepEqual(findAuthors(), authors);
     });
 
     it("find ids", function () {
-        assert.deepStrictEqual(findIDs(), authors);
+        assert.deepEqual(findIDs(), authors);
     });
 
     it("add book", function () {
         const newBook = {title: "My New Book", author: "Me Too", libraryID: 1144};
         ids.push(1144);
         ids.sort();
-        assert.deepStrictEqual(addBook(newBook), newBook);
+        assert.deepEqual(addBook(newBook), newBook);
         //assert.deepStrictEqual(findIDs(), ids);
     });
 
