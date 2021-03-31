834befd8c84367757ee834d02c20a7327e478b0d
KeithLevi <guilddrive@gmail.com>
2021-03-31 12:41:41 -0500

    class demos w2d3 am

--- w2d3higherOrder/myreduce.js
+++ w2d3higherOrder/myreduce.js
@@ -1,24 +1,32 @@
 "use strict";
 
-const arr = [1,2,3,4,5];
+const arr = [1, 2, 3, 4, 5];
 /**
  * 
  * @param {Array} arr to be processed
  * @param {*} cbk to map the elements
  * @returns {Array} of mapped elements
+ * make new array
+ * loop through arr
+ *  - call cbk(eachElement) and get return value and push it into new array
+ *  - return the resulting new array
  */
-function myMap(arr, cbk){
-//IMPLEMENTATION NEEDED
+function myMap(arr, cbk) {
+    const newArray = [];
+    for (let i = 0; i < arr.length; i++) {
+        newArray.push(cbk(arr[i], i));
+    }
+    return newArray;
 }
 
-const dblFun = num => num * 2;
+const dblFun = (num, index) => index + ": " + (num * 2);
 const squareFun = num => num * num;
 
 console.log("expect [2, 4, 6,8,10] ", arr.map(dblFun));
 console.log("expect [2, 4, 6,8,10] ", myMap(arr, dblFun));
 
-console.log("expect [4, 16, 36, 64, 100] ", arr.map(squareFun));
-console.log("expect [4, 16, 36, 64, 100] ", myMap(arr, squareFun));
+console.log("expect [1, 4, 9, 16, 25] ", arr.map(squareFun));
+console.log("expect [1, 4, 9, 16, 25] ", myMap(arr, squareFun));
 
 /**
  * 
@@ -30,8 +38,8 @@ console.log("expect [4, 16, 36, 64, 100] ", myMap(arr, squareFun));
  * go through the array, call cbk with accumulator and element
  * return value of cbk becomes accumulator for next loop
  */
-function myReduce(arr, cbk, initialVal){
-//IMPLEMENTATION NEEDED
+function myReduce(arr, cbk, initialVal) {
+    //IMPLEMENTATION NEEDED
 }
 
 const sumFun = (accum, val) => accum + val;
