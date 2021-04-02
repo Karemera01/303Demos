f87f31b247003981e4ce0de2967b1318796cd2e9
Keith Levi <guilddrive@gmail.com>
2021-04-02 08:15:28 -0500

    Update spread.js

--- mochaTests/labW2D6/spread.js
+++ mochaTests/labW2D6/spread.js
@@ -1,5 +1,5 @@
 "use strict";
-/* eslint-disable */
+
 /* You need the module.exports when testing in node.  Comment it out when you send your file to the browser 
 */
 //module.exports = {copyArray, concat, findMin, combineObjs }; //add all of your function names here that you need for the node mocha tests
@@ -7,17 +7,17 @@
 
 
 function copyArray(arr){
-     return [...arr];
+
 }
 
 function concat(arr1, arr2){
-    return [...arr1, ...arr2];
+
 }
 
 function findMin(...numbers) {
-    return Math.min(...numbers);
+
 }
 
 function combineObjs(obj1, obj2){
-    return {...obj1, ...obj2}; 
+
 }
